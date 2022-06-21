da092c6c4a77ee66bf53fab0da526499c90342d5
Amine SGHIR <amine.sghir@external.engie.com>
2022-06-21 10:45:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,18 +13,19 @@ class Model:
     def get_profiles(self):
         return db.profiles
     
-    def distance(self, profile, job_offer):
+    def score(self, profile, job_offer):
+        """représente le taux de ressemblance d'un job par rapport à un profil"""
         return 1 - len(set(profile['skills']) & set(job_offer)) / len(job_offer)
     
     def avg_score(self, job_offer):
-        return sum(self.distance(profile, job_offer) for profile in self.profiles.values()) / len(self.profiles)
+        return sum(self.score(profile, job_offer) for profile in self.profiles.values()) / len(self.profiles)
     
     def get_best_profiles(self, job_offer):
-        return sorted(self.profiles.values(), key=lambda profile: self.distance(profile, job_offer))
+        return sorted(self.profiles.values(), key=lambda profile: self.score(profile, job_offer))
     
 if __name__ == '__main__':
-    
+ 
     job_offer = random.sample(db.skills, random.randint(5, 20))
     model = Model()
-    print(job_offer)
+    print(job_offer, model.avg_score(job_offer))
     print(model.get_best_profiles(job_offer)[:5])
